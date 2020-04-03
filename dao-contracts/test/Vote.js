99546f77ba7a42363ac7d3ac0051dcc8347663fe
NikitaBerezhnoy <41240579+NickBer@users.noreply.github.com>
2020-04-03 19:18:38 +0300

    Merge commits

--- dao-contracts/test/Vote.js
+++ dao-contracts/test/Vote.js
@@ -1,5 +1,5 @@
 const ContributionRewardExt = artifacts.require("ContributionRewardExt");
-const AbsoluteVote = artifacts.require("GenesisProtocol");
+const GenesisProtocol = artifacts.require("GenesisProtocol");
 const Controller = artifacts.require("Controller");
 const Avatar = artifacts.require("Avatar");
 const SGTContract = artifacts.require("DAOToken");
@@ -8,25 +8,28 @@ const assert = require('assert').strict;
 const BN = web3.utils.BN;
 
 contract("Absolute vote", async accounts => {
-    const master = accounts[0];
+    const masterAccount = accounts[0];
     it(`It works`, async () => {
         let ContributionRewardExtInstance = await ContributionRewardExt.deployed();
-        let AbsoluteVoteInstance = await AbsoluteVote.deployed();
+        let GenesisProtocolInstance = await GenesisProtocol.deployed();
         let ControllerInstance = await Controller.deployed();
         let AvatarInstance = await Avatar.deployed();
         let SGTContractInstance = await SGTContract.deployed();
-        let proposalId = await ContributionRewardExtInstance.proposeContributionReward.call("Some description", 1, [2, 3, 4], SGTContractInstance.address, accounts[2], master);
-        console.log(proposalId);
+        let proposalId = await ContributionRewardExtInstance.proposeContributionReward.call("Some description", 1, [2, 3, 4], SGTContractInstance.address, accounts[2], masterAccount);
 
-        await ContributionRewardExtInstance.proposeContributionReward("Some description", 1, [2, 3, 4], SGTContractInstance.address, accounts[2], master);
+        await ContributionRewardExtInstance.proposeContributionReward("Some description", 1, [2, 3, 4], SGTContractInstance.address, accounts[2], masterAccount);
+
+        const ReputationContract = artifacts.require("Reputation");
+        // const ReputationInstance = await ReputationContract.deployed();
+        // for (let i = 0; i <= 5; i++) {
+        //     await ReputationInstance.mint(accounts[i], 100);
+        // }
         for (let i = 0; i <= 3; i++) {
             const acc = accounts[i];
-            await AbsoluteVoteInstance.vote(proposalId, 1, 0, acc, {
+            await GenesisProtocolInstance.vote(proposalId, 1, 0, acc, {
                 from: acc
             });
         }
-        console.log(await AvatarInstance.owner.call());
-        console.log(ControllerInstance.address);
         await ContributionRewardExtInstance.redeemReputation(proposalId);
         // assert(false);
     });
