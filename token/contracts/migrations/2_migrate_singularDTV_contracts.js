4edad63de69153b45c3769fd24b67fad9f756e8c
NikitaBerezhnoy <41240579+NickBer@users.noreply.github.com>
2020-06-01 16:30:37 +0300

    Add script for setting SNGLS in memfee

--- token/contracts/migrations/2_migrate_singularDTV_contracts.js
+++ token/contracts/migrations/2_migrate_singularDTV_contracts.js
@@ -6,5 +6,5 @@ module.exports = function (deployer, network, accounts) {
     console.log(accounts);
 
 
-    deployer.deploy(SingularDTVFund).then(() => deployer.deploy(SingularDTVToken, SingularDTVFund.address, '0xb1B2a95FC8815C341b684400227c3F3B1F43C376', 'SingularDTV', 'SNGLS', 100, accounts, [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100]));
+    deployer.deploy(SingularDTVFund).then(() => deployer.deploy(SingularDTVToken, SingularDTVFund.address, '0xb1B2a95FC8815C341b684400227c3F3B1F43C376', 'SingularDTV', 'SNGLS', 100, accounts, ["1000000000000000000000000", "900000000000000000000000", "800000000000000000000000", "777000000000000000000000", "600000000000000000000000", "500000000000000000000000", "400000000000000000000000", "300000000000000000000000", "200000000000000000000000", "100000000000000000000000"]));
 };
