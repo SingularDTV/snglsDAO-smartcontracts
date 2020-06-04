8c9e14e7c9a3c0821c1ed78bfc24ea4fe799fa44
NikitaBerezhnoy <41240579+NickBer@users.noreply.github.com>
2020-06-04 11:10:41 +0300

    Add script for emiting event on lock

---
+++ dao-contracts/scripts/lockTokensForSubgraph.js
@@ -0,0 +1,20 @@
+//script for emitting events for subgraph setup
+
+const LockingSGT4Reputation = artifacts.require("LockingSGT4Reputation");
+const sgtTokenContract = artifacts.require("DAOToken");
+const ReputationContract = artifacts.require("Reputation");
+const AvatarContract = artifacts.require("Avatar");
+
+const getDeployedAddress = require("../test/getDeployedAddress");
+contract("Locking tokens for subgraph setup", accounts => {
+    it(``, async () => {
+        let SGTContractInstance = await sgtTokenContract.at(await getDeployedAddress("DAOToken"));
+        let LT4RInstance = await LockingSGT4Reputation.at(await getDeployedAddress("LockingSGT4Reputation"));
+
+        const amount = 1;
+        await SGTContractInstance.approve(LT4RInstance.address, amount);
+        await LT4RInstance.lock(amount, 0);
+        console.log("\t--------- Locked ---------");
+
+    });
+})
