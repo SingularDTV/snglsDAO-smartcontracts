05eb757025d966e26d067eafd266844520ccece0
Konstantin Stepakh <konstantin.s@blaize.tech>
2020-06-26 17:14:08 +0300

    Cleanup code

--- dao-web-app/src/components/Dao/ThreeBoxTreadPage/ThreeBoxAddMessage.tsx
+++ dao-web-app/src/components/Dao/ThreeBoxTreadPage/ThreeBoxAddMessage.tsx
@@ -5,6 +5,7 @@ import { Comment, Form, Button, Input, Skeleton } from 'antd';
 import AccountImage from "../../Account/AccountImage";
 
 const { TextArea } = Input;
+const { Item } = Form;
 
 interface ThreeBoxAddMessageProps {
   thread: any;
@@ -14,19 +15,19 @@ interface ThreeBoxAddMessageProps {
 
 const ThreeBoxAddMessage =({ thread, profile, currentAddress }: ThreeBoxAddMessageProps) => {
   const [value, setValue] = useState('')
-  const [isSubmitting, seIsSubmittingt] = useState(false)
+  const [isSubmitting, seIsSubmitting] = useState(false)
 
   const handleChange = ({target: {value: val}}: any) => {
     setValue(val)
   };
 
   const handleSubmit = async () =>{
-    seIsSubmittingt(true)
+    seIsSubmitting(true)
     try{
       await thread.post(value)
       setValue('')
     } finally {
-      seIsSubmittingt(false)
+      seIsSubmitting(false)
     }
   }
 
@@ -36,14 +37,14 @@ const ThreeBoxAddMessage =({ thread, profile, currentAddress }: ThreeBoxAddMessa
       content={thread
         ? (
           <>
-            <Form.Item>
+            <Item>
               <TextArea rows={4} onChange={handleChange} value={value} />
-            </Form.Item>
-            <Form.Item>
+            </Item>
+            <Item>
               <Button htmlType="submit" loading={isSubmitting} disabled={isSubmitting || !value} onClick={handleSubmit} type="primary">
                 Add message
               </Button>
-            </Form.Item>
+            </Item>
           </>
         )
         : (<Skeleton />)
